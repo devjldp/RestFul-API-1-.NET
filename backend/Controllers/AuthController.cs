9b64f0cd7091a1aac5ad03fe17a8e3153f060b6c
jldpDev <joseluisdp297@gmail.com>
2025-05-07 16:12:01 +0100

    Add method to register a new admin

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -15,10 +15,32 @@ namespace Employees.Controllers
             _context = context;
         }
         
-        // Aquí irá el método de registro
+        // methods
 
+        [HttpPost("register")]
+        public IActionResult RegisterAdmin([FromBody] AdminUser newAdmin){
+            // Check if a user with the same username or email already exists
+            var existingUser = _context.AdminUsers.FirstOrDefault(u =>
+            u.UserName == newAdmin.UserName || u.Email == newAdmin.Email);
 
+            if (existingUser != null)
+            {
+                // returns a 400 BadRequest 
+                return BadRequest("User with the same username or email already exists.");
+            }
 
+            // Hash the plain text password using BCrypt
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(newAdmin.PasswordHash);
+
+            newAdmin.PasswordHash = hashedPassword;
+            
+            // Save the new admin user to the database -> EF Core methods
+            _context.AdminUsers.Add(newAdmin);
+            _context.SaveChanges();
+
+            // Returns a success response (200 OK) with a message to confirm registration.
+            return Ok(new { message = "Admin registered successfully." });
+        }
 
 
     }
--- backend/Models/AdminUser.cs
+++ backend/Models/AdminUser.cs
@@ -10,7 +10,7 @@ namespace Employees.Models
         [Required]
         public string LastName { get; set; } = string.Empty;
         [Required]
-        public string Username { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
         [Required]
         public string PasswordHash { get; set; } // Don't store the password as plain text
         [Required]
